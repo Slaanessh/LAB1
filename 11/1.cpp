fd1fac40a3cc27293f6ff2487f55666b82f4e0da
Slaanessh <arina0kolesnikova@gmail.com>
2024-10-06 22:27:07 +0300

    Добавка строки

--- 11/1.cpp
+++ 11/1.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 using namespace std;
+//Добавляю строку
+
 int main() {
-	setlocale(LC_ALL, "Russian");
-	cout << "Привет, мир!" << endl;
-	return 0;
+    setlocale(LC_ALL, "Russian");
+    double num1, num2;
+    char operation;
+
+    cout << "Введите первое число: ";
+    cin >> num1;
+
+    cout << "Введите операцию (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    switch (operation) {
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 == 0) {
+            cout << "Ошибка: Деление на ноль!" << endl;
+        }
+        else {
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+        }
+        break;
+    default:
+        cout << "Неверная операция!" << endl;
+    }
+
+    return 0;
 }
